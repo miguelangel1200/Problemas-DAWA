6dd8be3afd54c2b37dec619ccec192aec10e7e41
miguelangel1200 <75793818+miguelangel1200@users.noreply.github.com>
2022-09-02 08:51:11 -0500

    Add files via upload
    lab semana 2

---
+++ miguel-lab3/challengue7.py
@@ -0,0 +1,32 @@
+
+from ast import For
+from audioop import add
+from posixpath import split
+
+
+texto = input('Ingrese Texto: ')
+
+lista = []
+
+lista_comparacion = []
+# lista
+
+final=texto.replace(',','')
+test = final.split()
+
+print(test)
+
+while texto != '':
+    
+    for i in test:
+        if test == ' ':
+            break
+        vari = 
+    # vari = ''.join(map(str, test))
+    
+    lista.append(vari)
+    print(lista)
+        # print(i)
+
+
+    break
